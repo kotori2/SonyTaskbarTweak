8333bcb5e1febc2e5ff336918409351ad800b8c4
kotori0 <admin@0x.mk>
2022-01-23 00:17:12 +0800

    Initial

--- app/src/main/java/com/programminghoch10/UpsideWifi/UpsideWifi.java
+++ app/src/main/java/com/programminghoch10/UpsideWifi/UpsideWifi.java
@@ -1,27 +1,79 @@
 package com.programminghoch10.UpsideWifi;
 
 import android.os.Build;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 
 import de.robv.android.xposed.IXposedHookInitPackageResources;
+import de.robv.android.xposed.IXposedHookLoadPackage;
+import de.robv.android.xposed.XC_MethodHook;
+import de.robv.android.xposed.XposedBridge;
+import de.robv.android.xposed.XposedHelpers;
 import de.robv.android.xposed.callbacks.XC_InitPackageResources.InitPackageResourcesParam;
 import de.robv.android.xposed.callbacks.XC_LayoutInflated;
+import de.robv.android.xposed.callbacks.XC_LoadPackage;
 
 public class UpsideWifi implements IXposedHookInitPackageResources {
-    ImageView wifiIndicator;
+    ImageView volteIndicator;
 
     @Override
     public void handleInitPackageResources(InitPackageResourcesParam resparam) throws Throwable {
         if (!resparam.packageName.equals("com.android.systemui")) {
             return;
         }
-        String layoutName = android.os.Build.VERSION.SDK_INT >= Build.VERSION_CODES.P ? "status_bar_wifi_group" : "signal_cluster_view";
-        resparam.res.hookLayout("com.android.systemui", "layout", layoutName, new XC_LayoutInflated() {
+        resparam.res.hookLayout("com.android.systemui", "layout", "status_bar_mobile_signal_group", new XC_LayoutInflated() {
             @Override
             public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
-                wifiIndicator = liparam.view.findViewById(liparam.res.getIdentifier("wifi_signal", "id", "com.android.systemui"));
-                wifiIndicator.setRotation(180);
+                volteIndicator = liparam.view.findViewById(liparam.res.getIdentifier("volte", "id", "com.android.systemui"));
+                volteIndicator.setRotation(180);
+                volteIndicator.setImageResource(android.R.color.transparent);
+                ViewGroup.LayoutParams params = volteIndicator.getLayoutParams();
+                params.height = 0;
+                params.width = 0;
+                volteIndicator.setLayoutParams(params);
+            }
+        });
+        resparam.res.hookLayout("com.android.systemui", "layout", "status_bar_mobile_signal_group", new XC_LayoutInflated() {
+            @Override
+            public void handleLayoutInflated(LayoutInflatedParam liparam) throws Throwable {
+                volteIndicator = liparam.view.findViewById(liparam.res.getIdentifier("volte", "id", "com.android.systemui"));
+                volteIndicator.setRotation(180);
+                volteIndicator.setImageResource(android.R.color.transparent);
+                ViewGroup.LayoutParams params = volteIndicator.getLayoutParams();
+                params.height = 0;
+                params.width = 0;
+                volteIndicator.setLayoutParams(params);
             }
         });
     }
+
+    // Won't work for unknown reason
+    /*@Override
+    public void handleLoadPackage(XC_LoadPackage.LoadPackageParam lpparam) {
+        if (!lpparam.packageName.equals("com.android.systemui")) {
+            return;
+        }
+        Class MobileIconState;
+        try {
+            MobileIconState = Class.forName("com.android.systemui.statusbar.phone.StatusBarSignalPolicy$MobileIconState", false, lpparam.classLoader);
+        } catch (ClassNotFoundException ignored) {
+            XposedBridge.log("StatusBarSignalPolicy$MobileIconState not found!!!");
+            return;
+        }
+        XposedHelpers.findAndHookMethod("com.android.systemui.statusbar.StatusBarMobileView", lpparam.classLoader, "updateState",
+                MobileIconState,
+                new XC_MethodHook() {
+            @Override
+            protected void afterHookedMethod(MethodHookParam param) throws Throwable {
+                super.afterHookedMethod(param);
+                XposedBridge.log("StatusBarMobileView.updateState reset");
+                if (volteIndicator == null) {
+                    XposedBridge.log("StatusBarMobileView.updateState volteIndicator is null???");
+                }
+                volteIndicator.setVisibility(View.GONE);
+                volteIndicator.setImageResource(android.R.color.transparent);
+            }
+        });
+    }*/
 }
